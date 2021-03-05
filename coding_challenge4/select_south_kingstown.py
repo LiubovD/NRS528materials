9a8a6aa04f29aed15284e37e1c7de755840de578
Andy Davies <andy.marinebio@googlemail.com>
2021-03-05 15:46:27 -0500

    Feedback

--- coding_challenge4/select_south_kingstown.py
+++ coding_challenge4/select_south_kingstown.py
@@ -4,7 +4,7 @@ from arcpy import env
 env.workspace = "C:/Users/lubad/PycharmProjects/pythonProject1/Data"
 
 in_features = "towns.shp"
-out_feature_class = "C:/Users/lubad/PycharmProjects/pythonProject1/Data/select_south_kingstown.shp"
+out_feature_class = "select_south_kingstown.shp"
 where_clause = "NAME = 'SOUTH KINGSTOWN'"
 
 arcpy.Select_analysis(in_features, out_feature_class, where_clause)
