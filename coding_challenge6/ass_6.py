4a6de5ae44760766f9194c9c2cafc6f7bfc8bc0e
LiubovD <LiubovD@users.noreply.github.com>
2021-03-24 12:54:46 -0400

    Update ass_6.py

--- coding_challenge6/ass_6.py
+++ coding_challenge6/ass_6.py
@@ -8,7 +8,7 @@ listMonths = ["02", "04", "05", "07", "10", "11"]
 for month in listMonths:
     arcpy.env.workspace = "D:/Luba/pythonArcGIS/mod6/rasters/2015_" + month
     listRasters = arcpy.ListRasters("LC*", "TIF")
-#    print("For month: " + month + ", there are: " + str(len(listRasters)- 1) + "bands to process.")
+#    I am getting an error in here: for file in listRasters: TypeError: 'NoneType' object is not iterable
     for file in listRasters:
         if "_4.tif" in file:
             band_4 = file
